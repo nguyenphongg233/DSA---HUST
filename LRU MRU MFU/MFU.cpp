1501ff2dc49f0bacd649c2f810424bce2ee6e381
Nguyễn Phong <94354349+nguyenphongg233@users.noreply.github.com>
2025-06-11 22:19:42 +0700

    Update MFU.cpp

--- LRU MRU MFU/MFU.cpp
+++ LRU MRU MFU/MFU.cpp
@@ -1,32 +1,85 @@
 #include <bits/stdc++.h>
-// (Most Frequently Used)
-
 using namespace std;
 
 struct Node {
     int key;
     int cnt;
-    Node *prev;
-    Node *next;
+    Node *prev, *next;
     string value;
 
-    Node(int k, string val) : key(k), cnt(0), next(nullptr), prev(nullptr) {
-        value = val;
+    Node(int k, string val) : key(k), cnt(0), prev(nullptr), next(nullptr), value(val) {}
+};
+
+struct RowNode {
+    int freq;
+    RowNode* prev;
+    RowNode* next;
+
+    struct ListNode {
+        Node* data;
+        ListNode* prev;
+        ListNode* next;
+        ListNode(Node* node) : data(node), prev(nullptr), next(nullptr) {}
+    };
+
+    ListNode* head;
+    ListNode* tail;
+
+    RowNode(int f) : freq(f), prev(nullptr), next(nullptr), head(nullptr), tail(nullptr) {}
+
+    void push_front(Node* node) {
+        ListNode* newNode = new ListNode(node);
+        newNode->next = head;
+        if (head) head->prev = newNode;
+        head = newNode;
+        if (!tail) tail = newNode;
+    }
+
+    void pop_back() {
+        if (!tail) return;
+        ListNode* tmp = tail;
+        if (tail->prev) tail->prev->next = nullptr;
+        tail = tail->prev;
+        if (!tail) head = nullptr;
+        delete tmp;
+    }
+
+    Node* back() {
+        return tail ? tail->data : nullptr;
+    }
+
+    bool empty() {
+        return head == nullptr;
     }
 };
 
 struct MFU_Cache {
-    vector<deque<Node*>> save;
+    RowNode* saveHead = nullptr;
     unordered_map<int, Node*> cacheMap;
-    int Max;
-    int sz;
+    int Max = 0;
+    int sz = 0;
     int lim;
-    int Max_num_inter;
-    Node *head;
-    Node *tail;
+    Node *head = nullptr, *tail = nullptr;
 
-    MFU_Cache(int LIM) : Max(0), sz(0), lim(LIM), head(nullptr), tail(nullptr) {
-        save = vector<deque<Node*>>(5);
+    MFU_Cache(int LIM) : lim(LIM) {}
+
+    // Tìm hoặc tạo hàng với tần suất f
+    RowNode* getRow(int f) {
+        RowNode* cur = saveHead;
+        RowNode* last = nullptr;
+        while (cur) {
+            if (cur->freq == f) return cur;
+            last = cur;
+            cur = cur->next;
+        }
+        RowNode* newRow = new RowNode(f);
+        if (!saveHead) {
+            saveHead = newRow;
+        } else {
+            last->next = newRow;
+            newRow->prev = last;
+        }
+        return newRow;
     }
 
     void removeNode(Node* node) {
@@ -54,56 +107,50 @@ struct MFU_Cache {
     }
 
     void del_Max() {
-        Node *need_del;
-        bool ok = 0;
-        while (!ok) {
-            if (save[Max].size() == 0) {
-                Max--;
-                continue;
-            }
-            Node *tmp = save[Max].back();
-            if (tmp->cnt == Max) {
-                ok = 1;
-                need_del = tmp;
-            }
-            save[Max].pop_back();
+        RowNode* cur = saveHead;
+        while (cur && cur->freq != Max) cur = cur->next;
+        while (cur && cur->empty()) {
+            cur = cur->prev;
+            Max--;
         }
+
+        if (!cur) return;
+
+        Node* need_del = cur->back();
+        cur->pop_back();
         cacheMap.erase(need_del->key);
-        need_del->cnt = 0;
         removeNode(need_del);
     }
 
     void putCache(int key, string value) {
         auto it = cacheMap.find(key);
         if (it != cacheMap.end()) {
-            Node *node = (*it).second;
+            Node* node = it->second;
             node->cnt++;
-            if (Max < node->cnt) Max = node->cnt;
-            if (Max > save.size() - 2) save.push_back(deque<Node*>(0));
-            save[node->cnt].push_front(node);
+            Max = max(Max, node->cnt);
+            RowNode* row = getRow(node->cnt);
+            row->push_front(node);
             moveNodeToHead(node);
         } else {
-            if (sz == lim) {
-                del_Max();
-            }
+            if (sz == lim) del_Max();
             Node* node = new Node(key, value);
+            node->cnt = 1;
+            Max = max(Max, 1);
+            RowNode* row = getRow(1);
+            row->push_front(node);
             addNodeToHead(node);
             cacheMap[key] = node;
-            node->cnt = 1;
-            if (Max < node->cnt) Max = node->cnt;
-            if (Max > save.size() - 2) save.push_back(deque<Node*>(0));
-            save[node->cnt].push_front(node);
         }
     }
 
     void getCache(int key) {
         auto it = cacheMap.find(key);
         if (it != cacheMap.end()) {
-            Node *node = (*it).second;
+            Node* node = it->second;
             node->cnt++;
-            if (Max < node->cnt) Max = node->cnt;
-            if (Max > save.size() - 2) save.push_back(deque<Node*>(0));
-            save[node->cnt].push_front(node);
+            Max = max(Max, node->cnt);
+            RowNode* row = getRow(node->cnt);
+            row->push_front(node);
             moveNodeToHead(node);
         }
     }
@@ -119,6 +166,7 @@ struct MFU_Cache {
     }
 };
 
+// Giao diện tương tự như API
 MFU_Cache* initCache(int lim) {
     return new MFU_Cache(lim);
 }
@@ -135,21 +183,22 @@ void printCache(MFU_Cache* cache) {
     cache->printCache();
 }
 
-signed main() {
+// ===================== MAIN ========================
+int main() {
     int CACHE_SIZE = 3;
     MFU_Cache* cache = initCache(CACHE_SIZE);
-    
+
     put(cache, 1, "Value 1");
     put(cache, 2, "Value 2");
     put(cache, 3, "Value 3");
-    printCache(cache);
+    printCache(cache); // Cache: (3,Value 3) (2,Value 2) (1,Value 1)
 
     get(cache, 2); 
-    printCache(cache);
+    printCache(cache); // Cache: (2,Value 2) (3,Value 3) (1,Value 1)
 
-    put(cache, 4, "Value 4");
-    printCache(cache);
+    put(cache, 4, "Value 4"); // Xoá node có freq cao nhất
+    printCache(cache); // Cache: (4,Value 4) (2,Value 2) (3 hoặc 1)
 
-    get(cache, 1);
+    get(cache, 1); // nếu còn
     return 0;
 }
